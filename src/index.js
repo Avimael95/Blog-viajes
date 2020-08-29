96cba13a7d628567add57ce4493b8f8139308507
Avimael <ing.avimelgr95@gmail.com>
2020-08-28 23:06:39 -0500

    Blog de viajes v1.1.0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@
 // require('./styles/main.scss');
 //otra forma de cargar los archivos
 import './styles/main.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+import './static/logo.png';
+import './static/01.jpg';
+import './static/02.jpg';
+import './static/03.jpg';
+import './static/04.jpg';
+import './static/05.jpg';
+import './static/06.jpg';
+
+
+
+
+
 
 
 
